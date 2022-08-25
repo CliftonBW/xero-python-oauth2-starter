39d0ac8129b25e716065bec0e3400a00737640f5
Clifton Ngeow <81543516+CliftonBW@users.noreply.github.com>
2022-08-25 13:15:36 +0800

    test
    test

---
+++ config.py
@@ -0,0 +1,9 @@
+from os import environ, path
+from pathlib import Path
+
+CLIENT_ID = environ['CLIENT_ID']
+CLIENT_SECRET = environ['CLIENT_SECRET']
+LINEITEM_CODE = environ['LINEITEM_CODE']
+INVOICE_CODE = environ['INVOICE_CODE']
+EMAIL_CODE = environ['EMAIL_CODE']
+INVOICES_CODE = environ['INVOICES_CODE']
