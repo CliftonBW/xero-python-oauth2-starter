4562184d14965c84f20e25e32d83a58f5d1875a6
Clifton Ngeow <81543516+CliftonBW@users.noreply.github.com>
2023-07-26 10:35:05 +0800

    venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\clift\OneDrive\Documents\GitHub\xero-python-oauth2-starter\.venv
+set VIRTUAL_ENV=C:\Users\CliftonNg\Documents\GitHub\xero-python-oauth2-starter\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
